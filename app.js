1ce7ec4735868261c8ece7b092ad4bec100b30dc
Sohan Pinnarwar <mr.sohanpinnarwar@gmail.com>
2025-07-11 15:15:59 +0530

    Add app.js for today's pratice

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const  express = require("express")
-
+const express = require("express");
 const app = express();
 
-app.use("/home",(req, res) => {
+// this will match only GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstname: "sohan", lastname: "Pinnarwar" });
+});
+
+// this will match only POST call to /user
+app.post("/user", (req, res) => {
+    res.send("Data successfully saved to the database");
+});
+
+// this will match only DELETE call to /user
+app.delete("/user", (req, res) => {
+    res.send("User profile deleted successfully");
+});
+
+// this will match all the HTTP method API calls to /home
+app.use("/home", (req, res) => {
     res.send("Welcome to home page");
 });
 
-app.use("/profile",(req,res) =>{
-    res.send("Hello sohan ");
+
+
+// dynamic route using route parameters
+app.get("/test/:id", (req, res) =>{
+    const userid = req.params.id;
+    console.log(`user id is :  ${userid}`);
 });
 
-
-app.use("/orders",(req, res) =>{
-    res.send("Welcome to my orders");
+// OPTIONAL — general /user route moved to the end
+app.use("/user", (req, res) => {
+    res.send("testing......");
 });
-
 
 app.listen(3000, () => {
-    console.log("server started listing at port 3000");
+    console.log("server started listening at port 3000");
 });
